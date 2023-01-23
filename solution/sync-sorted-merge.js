c25df464a3a4007d3d67f3d9262205cbc64052dc
Melissa Farrell <caretcake@outlook.com>
2023-01-23 16:36:44 -0500

    Implement sync solution

--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,7 +1,23 @@
 "use strict";
+const { Heap } = require('heap-js');
 
 // Print all entries, across all of the sources, in chronological order.
 
 module.exports = (logSources, printer) => {
+  const logHeap = new Heap((a, b) => a.last.date.getTime() - b.last.date.getTime());
+  logHeap.init(logSources);
+
+  while(logHeap.size() > 0) {
+    const currentLog = logHeap.pop();
+
+    printer.print(currentLog.last);
+    currentLog.pop();
+    
+    if (!currentLog.drained) {
+      logHeap.push(currentLog);
+    }
+  }
+
+  printer.done();
   return console.log("Sync sort complete.");
 };
